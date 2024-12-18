7225405817be65341dd92ac164a2ec3325508ed7
alan <alan.266@hotmail.com>
2024-12-18 15:47:49 -0400

    updates

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -4,7 +4,7 @@ import { createUserWithEmailAndPassword } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 import Modal from "react-modal";
 
-Modal.setAppElement("#root"); // Necesario para react-modal
+Modal.setAppElement("#root");
 
 const Register = () => {
   const [email, setEmail] = useState("");
@@ -44,7 +44,10 @@ const Register = () => {
       if (response.ok) {
         setIsSuccess(true); // Mostrar modal de éxito
       } else {
-        setErrorMessage("Error al guardar datos adicionales en el servidor.");
+        const errorResponse = await response.json();
+        setErrorMessage(
+          errorResponse.message || "Error al guardar datos adicionales."
+        );
       }
     } catch (error) {
       if (error.code === "auth/email-already-in-use") {
@@ -69,71 +72,93 @@ const Register = () => {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <div className="bg-white p-8 rounded-lg shadow-lg w-96">
-        <h2 className="text-2xl font-bold text-red-600 text-center mb-6">
-          Registro de Usuario
-        </h2>
-        <form onSubmit={handleRegister}>
-          <div className="mb-4">
-            <label className="block text-gray-700">Nombre Completo</label>
-            <input
-              type="text"
-              className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-red-400"
-              placeholder="John Doe"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              required
-            />
-          </div>
-          <div className="mb-4">
-            <label className="block text-gray-700">Correo Electrónico</label>
-            <input
-              type="email"
-              className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-red-400"
-              placeholder="correo@example.com"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-          </div>
-          <div className="mb-4">
-            <label className="block text-gray-700">Número de Teléfono</label>
-            <input
-              type="tel"
-              className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-red-400"
-              placeholder="809-123-4567"
-              value={phone}
-              onChange={(e) => setPhone(e.target.value)}
-              required
-            />
-          </div>
-          <div className="mb-6">
-            <label className="block text-gray-700">Contraseña</label>
-            <input
-              type="password"
-              className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-red-400"
-              placeholder="********"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-          </div>
-
-          {/* Mostrar mensaje de error */}
-          {errorMessage && (
-            <div className="text-red-600 text-sm mb-4 text-center">
-              {errorMessage}
-            </div>
-          )}
-
+    <div className="min-h-screen flex flex-col bg-gray-100">
+      {/* Header */}
+      <header className="bg-red-600 text-white px-6 py-4 flex items-center justify-between">
+        <h1 className="text-2xl font-bold">Taxes247</h1>
+        <div>
           <button
-            type="submit"
-            className="w-full bg-red-600 text-white py-2 rounded-md hover:bg-red-700"
+            className="bg-red-700 mr-4 px-4 py-2 hover:bg-red-800  text-left rounded-md"
+            onClick={() => navigate("/")}
           >
-            Registrarse
+            Login
           </button>
-        </form>
+          <button
+            onClick={() => window.open("https://wa.me/18094039726", "_blank")}
+            className="bg-white text-red-600 px-4 py-2 rounded-md hover:bg-gray-200"
+          >
+            Contacto
+          </button>
+        </div>
+      </header>
+
+      {/* Formulario de Registro */}
+      <div className="flex-1 flex items-center justify-center">
+        <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
+          <h2 className="text-2xl font-bold text-red-600 text-center mb-6">
+            Registro de Usuario
+          </h2>
+          <form onSubmit={handleRegister}>
+            <div className="mb-4">
+              <label className="block text-gray-700">Nombre Completo</label>
+              <input
+                type="text"
+                className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-red-400"
+                placeholder="John Doe"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                required
+              />
+            </div>
+            <div className="mb-4">
+              <label className="block text-gray-700">Correo Electrónico</label>
+              <input
+                type="email"
+                className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-red-400"
+                placeholder="correo@example.com"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </div>
+            <div className="mb-4">
+              <label className="block text-gray-700">Número de Teléfono</label>
+              <input
+                type="tel"
+                className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-red-400"
+                placeholder="809-123-4567"
+                value={phone}
+                onChange={(e) => setPhone(e.target.value)}
+                required
+              />
+            </div>
+            <div className="mb-6">
+              <label className="block text-gray-700">Contraseña</label>
+              <input
+                type="password"
+                className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-red-400"
+                placeholder="********"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </div>
+
+            {/* Mostrar mensaje de error */}
+            {errorMessage && (
+              <div className="text-red-600 text-sm mb-4 text-center">
+                {errorMessage}
+              </div>
+            )}
+
+            <button
+              type="submit"
+              className="w-full bg-red-600 text-white py-2 rounded-md hover:bg-red-700"
+            >
+              Registrarse
+            </button>
+          </form>
+        </div>
       </div>
 
       {/* Modal de Éxito */}
